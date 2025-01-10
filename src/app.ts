0352abaa88ed1f5a52acff63680032b26ac7a213
sonuzero <thebraindrop.zero@gmail.com>
2025-01-10 14:15:47 +0530

    project init

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    credentials: true
+  })
+);
+app.use(morgan("dev"));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
+
+import routes from "./routes/index";
+app.use("/api/v1", routes);
+
+export { app };
